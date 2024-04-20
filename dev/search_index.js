0b78dd52fc0bb20a150fcf0aae00e0d229f5cf06
Documenter.jl <documenter@juliadocs.github.io>
2024-04-20 13:19:13 +0000

    build based on fb31b84

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TeNe.jl-Documentation","page":"TeNe.jl Documentation","title":"TeNe.jl Documentation","text":"","category":"section"},{"location":"","page":"TeNe.jl Documentation","title":"TeNe.jl Documentation","text":"```@docs contract(x, y, cix, ciy) '''","category":"page"}]
+}
