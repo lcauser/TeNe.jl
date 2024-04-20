16a74f212e8e90ad702aeb01562d5fa66b4f4046
alsoblank <luke.causer@outlook.com>
2024-04-20 14:12:14 +0100

    Redone documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,4 @@
 push!(LOAD_PATH,"../src/")
-using TeNe
-using Documenter
-makedocs(
-         sitename = "TeNe.jl",
-         modules  = [TeNe],
-         pages=[
-                "Home" => "index.md"
-               ])
-deploydocs(;
-    repo="github.com/lcauser/TeNe.jl",
-)
+using Documenter, TeNe
+
+makedocs(sitename="TeNe.jl")
