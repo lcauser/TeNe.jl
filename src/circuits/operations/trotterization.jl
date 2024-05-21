a00b94b5ed11da595e0cd9df99c4cc1fd6dce066
alsoblank <luke.causer@outlook.com>
2024-05-21 20:31:00 +0100

    added code for creating gates for compressed trotterization

--- src/circuits/operations/trotterization.jl
+++ src/circuits/operations/trotterization.jl
@@ -7,17 +7,36 @@ function trotterize(H::OpList, t::Number; order::Int=2, type::Symbol=:compressed
 end
 
 
-### Creating the Trotter gate 
+### Creating the Trotter gate in a compressed way
 
-function _create_trotter_gate(H::OpList, t::Number, site::Int)
+function _create_trotter_gate_compressed(H::OpList, t::Number, site::Int)
     # Create a tensor to store the result
     rng = siterange(H) 
     ten = zeros(eltype(H.lt), [dim(H) for _ = 1:2*rng]...)
 
-    # Find all the gates that start at the site with the correct range 
-    rng = siterange(H)
-    idxs = siteindexs(H, site)
-    
+    # Loop through all sites and find the operators in the range 
+    for i in Base.OneTo(rng)
+        # Find operators which start at this index
+        idxs = siteindexs(H, site + i - 1)
+        for idx in idxs 
+            # Make sure the operator has small enough range 
+            rng_local = H.sites[idx][end] - H.sites[idx][begin] + 1
+            padding = rng - (i - 1 + rng_local)
+            if padding >= 0
+                # Determine the coefficient to ensure the term is not over counted 
+                first_site = max(site + i - 1 + rng_local - rng, 1)
+                last_site = min(length(H) - rng + 1, site + i - 1)
+                coeff = last_site - first_site + 1
+                
+                # Find the operator 
+                op = totensor(H, idx, i-1, padding; tocache=true)
+                op .*= (1 / coeff)
+                ten .+= op
+            end
+        end
+    end
 
-
+    # Exponentiate the tensor 
+    ten = TeNe.exp(ten, Tuple(Base.range(2, 2*rng, step=2)); prefactor=t)
+    return ten    
 end
---
+++ test/circuits/trotterization.jl
@@ -0,0 +1,114 @@
+@testset "Trotterization" begin
+    # Testing _create_trotter_gate_compressed
+    @testset "_create_trotter_gate_compressed" begin
+        @testset "two sites" begin
+            H = OpList(Qubits(), 20)
+            for i = 1:20
+                add!(H, "x", i, 0.34)
+            end
+            for i = 1:19
+                add!(H, ["z", "z"], [i, i+1], 0.56)
+            end
+
+            @test begin
+                op = TeNe._create_trotter_gate_compressed(H, 0.1, 5)
+                op2 = 0.56*tensorproduct([1 0; 0 -1], [1 0; 0 -1]; tocache=false)
+                op2 += 0.5*0.34*tensorproduct([0 1; 1 0], [1 0; 0 1]; tocache=false)
+                op2 += 0.5*0.34*tensorproduct([1 0; 0 1], [0 1; 1 0]; tocache=false)
+                op2 = TeNe.exp(0.1*op2, (2, 4))
+                isapprox(op, op2)
+            end
+
+            @test begin
+                op = TeNe._create_trotter_gate_compressed(H, 0.1, 1)
+                op2 = 0.56*tensorproduct([1 0; 0 -1], [1 0; 0 -1]; tocache=false)
+                op2 += 0.34*tensorproduct([0 1; 1 0], [1 0; 0 1]; tocache=false)
+                op2 += 0.5*0.34*tensorproduct([1 0; 0 1], [0 1; 1 0]; tocache=false)
+                op2 = TeNe.exp(0.1*op2, (2, 4))
+                isapprox(op, op2)
+            end
+
+            @test begin
+                op = TeNe._create_trotter_gate_compressed(H, 0.1, 19)
+                op2 = 0.56*tensorproduct([1 0; 0 -1], [1 0; 0 -1]; tocache=false)
+                op2 += 0.5*0.34*tensorproduct([0 1; 1 0], [1 0; 0 1]; tocache=false)
+                op2 += 0.34*tensorproduct([1 0; 0 1], [0 1; 1 0]; tocache=false)
+                op2 = TeNe.exp(0.1*op2, (2, 4))
+                isapprox(op, op2)
+            end
+        end
+
+        @testset "three sites" begin
+            H = OpList(Qubits(), 20)
+            for i = 1:20
+                add!(H, "x", i, 0.34)
+            end
+            for i = 1:19
+                add!(H, ["z", "z"], [i, i+1], 0.56)
+            end
+            for i = 1:18
+                add!(H, ["z", "x", "z"], [i, i+1, i+2], -1.4)
+            end
+
+            @test begin
+                op = TeNe._create_trotter_gate_compressed(H, 0.1, 5)
+                op2 = -1.4*tensorproduct(tensorproduct([1 0; 0 -1], [0 1; 1 0]; tocache=false), [1 0; 0 -1]; tocache=false)
+                op2 += 0.56/2*tensorproduct(tensorproduct([1 0; 0 -1], [1 0; 0 -1]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.56/2*tensorproduct(tensorproduct([1 0; 0 1], [1 0; 0 -1]; tocache=false), [1 0; 0 -1]; tocache=false)
+                op2 += 0.34/3*tensorproduct(tensorproduct([0 1; 1 0], [1 0; 0 1]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.34/3*tensorproduct(tensorproduct([1 0; 0 1], [0 1; 1 0]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.34/3*tensorproduct(tensorproduct([1 0; 0 1], [1 0; 0 1]; tocache=false), [0 1; 1 0]; tocache=false)
+                op2 = TeNe.exp(0.1*op2, (2, 4, 6))
+                isapprox(op, op2)
+            end
+
+            @test begin
+                op = TeNe._create_trotter_gate_compressed(H, 0.1, 1)
+                op2 = -1.4*tensorproduct(tensorproduct([1 0; 0 -1], [0 1; 1 0]; tocache=false), [1 0; 0 -1]; tocache=false)
+                op2 += 0.56*tensorproduct(tensorproduct([1 0; 0 -1], [1 0; 0 -1]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.56/2*tensorproduct(tensorproduct([1 0; 0 1], [1 0; 0 -1]; tocache=false), [1 0; 0 -1]; tocache=false)
+                op2 += 0.34*tensorproduct(tensorproduct([0 1; 1 0], [1 0; 0 1]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.34/2*tensorproduct(tensorproduct([1 0; 0 1], [0 1; 1 0]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.34/3*tensorproduct(tensorproduct([1 0; 0 1], [1 0; 0 1]; tocache=false), [0 1; 1 0]; tocache=false)
+                op2 = TeNe.exp(0.1*op2, (2, 4, 6))
+                isapprox(op, op2)
+            end
+
+            @test begin
+                op = TeNe._create_trotter_gate_compressed(H, 0.1, 2)
+                op2 = -1.4*tensorproduct(tensorproduct([1 0; 0 -1], [0 1; 1 0]; tocache=false), [1 0; 0 -1]; tocache=false)
+                op2 += 0.56/2*tensorproduct(tensorproduct([1 0; 0 -1], [1 0; 0 -1]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.56/2*tensorproduct(tensorproduct([1 0; 0 1], [1 0; 0 -1]; tocache=false), [1 0; 0 -1]; tocache=false)
+                op2 += 0.34/2*tensorproduct(tensorproduct([0 1; 1 0], [1 0; 0 1]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.34/3*tensorproduct(tensorproduct([1 0; 0 1], [0 1; 1 0]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.34/3*tensorproduct(tensorproduct([1 0; 0 1], [1 0; 0 1]; tocache=false), [0 1; 1 0]; tocache=false)
+                op2 = TeNe.exp(0.1*op2, (2, 4, 6))
+                isapprox(op, op2)
+            end
+
+            @test begin
+                op = TeNe._create_trotter_gate_compressed(H, 0.1, 18)
+                op2 = -1.4*tensorproduct(tensorproduct([1 0; 0 -1], [0 1; 1 0]; tocache=false), [1 0; 0 -1]; tocache=false)
+                op2 += 0.56/2*tensorproduct(tensorproduct([1 0; 0 -1], [1 0; 0 -1]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.56*tensorproduct(tensorproduct([1 0; 0 1], [1 0; 0 -1]; tocache=false), [1 0; 0 -1]; tocache=false)
+                op2 += 0.34/3*tensorproduct(tensorproduct([0 1; 1 0], [1 0; 0 1]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.34/2*tensorproduct(tensorproduct([1 0; 0 1], [0 1; 1 0]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.34*tensorproduct(tensorproduct([1 0; 0 1], [1 0; 0 1]; tocache=false), [0 1; 1 0]; tocache=false)
+                op2 = TeNe.exp(0.1*op2, (2, 4, 6))
+                isapprox(op, op2)
+            end
+
+            @test begin
+                op = TeNe._create_trotter_gate_compressed(H, 0.1, 17)
+                op2 = -1.4*tensorproduct(tensorproduct([1 0; 0 -1], [0 1; 1 0]; tocache=false), [1 0; 0 -1]; tocache=false)
+                op2 += 0.56/2*tensorproduct(tensorproduct([1 0; 0 -1], [1 0; 0 -1]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.56/2*tensorproduct(tensorproduct([1 0; 0 1], [1 0; 0 -1]; tocache=false), [1 0; 0 -1]; tocache=false)
+                op2 += 0.34/3*tensorproduct(tensorproduct([0 1; 1 0], [1 0; 0 1]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.34/3*tensorproduct(tensorproduct([1 0; 0 1], [0 1; 1 0]; tocache=false), [1 0; 0 1]; tocache=false)
+                op2 += 0.34/2*tensorproduct(tensorproduct([1 0; 0 1], [1 0; 0 1]; tocache=false), [0 1; 1 0]; tocache=false)
+                op2 = TeNe.exp(0.1*op2, (2, 4, 6))
+                isapprox(op, op2)
+            end
+        end
+    end
+end
