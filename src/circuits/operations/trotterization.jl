6c11fff83e5ab0fa60fc220e1a83b2b89530fec9
Luke Causer <luke.causer@outlook.com>
2024-06-04 11:51:28 +0100

    Added Trotterization of circuits

--- src/circuits/operations/trotterization.jl
+++ src/circuits/operations/trotterization.jl
@@ -2,12 +2,81 @@
     Creates a circuit for time evolution using Trotterization
 =#
 
-function trotterize(H::OpList, t::Number; order::Int=2, type::Symbol=:compressed)
+"""
+    trotterize(H::OpList, t::Number; kwargs...)
 
+Trotterize the Hamiltonian `H` for time step `t`. That is, approximate exp(tH) by
+a discrete circuit.
+
+# Optional Keyword Arguments 
+
+    - `order::Int=2`: The order of the approximation; either `1` or `2`.
+    - `type::Symbol=:compressed`: Use `uncompressed` for a raw circuit. Use `compressed`
+       to treat terms with a smaller site range as a term with a larger range.
+       In theory, `compressed` should give some prefactor improvements on the error.
+
+# Examples 
+
+```julia-repl
+julia> H = OpList(Qubits(), 20);
+julia> for i = 1:20 add!(H, "x", i, 0.34); end
+julia> for i = 1:19 add!(H, ["z", "z"], [i, i+1], 0.56); end
+julia> circuit = trotterize(H, -0.01im);
+```
+"""
+function trotterize(H::OpList, t::Number; order::Int=2, type::Symbol=:compressed)
+    # Validation on the hyperparameters 
+    if !(order > 0 && order <= 2)
+        throw(ArgumentError("Only first and second order are currently supported."))
+    end
+    if type != :compressed && type != :uncompressed
+        throw(ArgumentError("The type of circuit must be either compressed or uncompressed."))
+    end
+
+    # Choose the correct trotterization 
+    if type == :compressed && order == 1
+        return _trotter_compressed_first_order(H, t)
+    elseif type == :compressed && order == 2
+        return _trotter_compressed_second_order(H, t)
+    elseif  type == :uncompressed && order == 1
+        return _trotter_uncompressed_first_order(H, t)
+    else
+        return _trotter_uncompressed_second_order(H, t)
+    end
 end
+export trotterize
 
 
 ### Creating the Trotter gate in a compressed way
+# First-order decomposition with compressed gates
+function _trotter_compressed_first_order(H::OpList, t::Number)
+    # Create the circuit 
+    circuit = Circuit(dim(H), length(H))
+
+    # Add the layers 
+    for i = 1:siterange(H)
+        push!(circuit.layers, _create_trotter_layer_compressed(H, t, i))
+    end
+    return circuit
+end
+
+# Second-order decomposition with compressed gates 
+function _trotter_compressed_second_order(H::OpList, t::Number)
+    # Create the circuit 
+    circuit = Circuit(dim(H), length(H))
+
+    # Add the layers 
+    for i = 1:siterange(H)-1
+        push!(circuit.layers, _create_trotter_layer_compressed(H, t/2, i))
+    end
+    push!(circuit.layers, _create_trotter_layer_compressed(H, t, siterange(H)))
+    for i = Base.range(siterange(H)-1, 1, step=-1)
+        push!(circuit.layers, circuit.layers[i])
+    end
+    return circuit
+end
+
+# Create a compressed gate that starts at ``site``
 function _create_trotter_gate_compressed(H::OpList, t::Number, site::Int)
     # Create a tensor to store the result
     rng = siterange(H) 
@@ -40,7 +109,51 @@ function _create_trotter_gate_compressed(H::OpList, t::Number, site::Int)
     return ten    
 end
 
+# Create a layer of compressed gates; parity specifies the sequence of sites.
+function _create_trotter_layer_compressed(H::OpList, t::Number, parity::Int)
+    rng = siterange(H)
+    layer = CircuitLayer(dim(H), length(H))
+    for j = Base.range(parity, length(H)-rng+1, step=rng)
+        add!(layer, creategate(_create_trotter_gate_compressed(H, t, j)),
+            Tuple(Base.range(j, j+rng-1)))
+    end
+    return layer
+end
+
 ### Creating the trotter gate without compression 
+# First-order decomposition with uncompressed gates
+function _trotter_uncompressed_first_order(H::OpList, t::Number)
+    # Create the circuit 
+    circuit = Circuit(dim(H), length(H))
+
+    # Add the layers 
+    for k = 1:siterange(H)
+        for i = 1:k
+            push!(circuit.layers, _create_trotter_layer_uncompressed(H, t, i, k))
+        end
+    end
+    return circuit
+end
+
+# Second-order decomposition with uncompressed gates 
+function _trotter_uncompressed_second_order(H::OpList, t::Number)
+    # Create the circuit 
+    circuit = Circuit(dim(H), length(H))
+
+    # Add the layers 
+    for k = 1:siterange(H)
+        for i = 1:min(k, siterange(H)-1)
+            push!(circuit.layers, _create_trotter_layer_uncompressed(H, t/2, i, k))
+        end
+    end
+    push!(circuit.layers, _create_trotter_layer_uncompressed(H, t, siterange(H), siterange(H)))
+    for k = Base.range(length(circuit.layers)-1, 1, step=-1)
+        push!(circuit.layers, circuit.layers[k])
+    end
+    return circuit
+end
+
+# Creating an uncompressed gate 
 function _create_trotter_gate_uncompressed(H::OpList, t::Number, site::Int, rng::Int)
     # Create a tensor to store the result 
     ten = zeros(eltype(H.lt), [dim(H) for _ = 1:2*rng]...)
@@ -58,4 +171,14 @@ function _create_trotter_gate_uncompressed(H::OpList, t::Number, site::Int, rng:
     # Exponentiate the tensor 
     ten = TeNe.exp(ten, Tuple(Base.range(2, 2*rng, step=2)); prefactor=t)
     return ten  
+end
+
+# Create a layer of uncompressed gates; parity specifies the sequence of sites.
+function _create_trotter_layer_uncompressed(H::OpList, t::Number, parity::Int, rng::Int)
+    layer = CircuitLayer(dim(H), length(H))
+    for j = Base.range(parity, length(H)-rng+1, step=rng)
+        add!(layer, creategate(_create_trotter_gate_uncompressed(H, t, j, rng)),
+            Tuple(Base.range(j, j+rng-1)))
+    end
+    return layer
 end
--- test/circuits/trotterization.jl
+++ test/circuits/trotterization.jl
@@ -125,10 +125,85 @@
 
             @test begin
                 op = TeNe._create_trotter_gate_uncompressed(H, 0.1, 5, 1)
-                op2 = TeNe.exp(0.34*0.1*[0.0 1; 1 0], (2))
+                op2 = TeNe.exp(0.34*0.1*[0 1; 1 0], (2))
                 isapprox(op, op2)
-                true
+            end
+
+            @test begin
+                op = TeNe._create_trotter_gate_uncompressed(H, -0.1im, 2, 2)
+                op2 = tensorproduct([1 0; 0 -1], [1 0; 0 -1]; tocache=false)
+                op2 = TeNe.exp(-0.56*0.1im*op2, (2, 4))
+                isapprox(op, op2)
+            end
+        end
+
+        @testset "three sites" begin 
+            H = OpList(Qubits(), 20)
+            for i = 1:20
+                add!(H, "x", i, 0.34)
+            end
+            for i = 1:19
+                add!(H, ["z", "z"], [i, i+1], 0.56)
+            end
+            for i = 1:18
+                add!(H, ["z", "x", "z"], [i, i+1, i+2], -1.4)
+            end
+
+            @test begin
+                op = TeNe._create_trotter_gate_uncompressed(H, 0.1, 5, 1)
+                op2 = TeNe.exp(0.34*0.1*[0 1; 1 0], (2))
+                isapprox(op, op2)
+            end
+            @test begin
+                op = TeNe._create_trotter_gate_uncompressed(H, -0.1im, 2, 2)
+                op2 = tensorproduct([1 0; 0 -1], [1 0; 0 -1]; tocache=false)
+                op2 = TeNe.exp(-0.56*0.1im*op2, (2, 4))
+                isapprox(op, op2)
+            end
+
+            @test begin
+                op = TeNe._create_trotter_gate_uncompressed(H, -0.1im, 8, 3)
+                op2 = tensorproduct(tensorproduct([1 0; 0 -1], [0 1; 1 0]; tocache=false), [1 0; 0 -1]; tocache=false)
+                op2 = TeNe.exp(1.4*0.1im*op2, (2, 4, 6))
+                isapprox(op, op2)
             end
         end
     end
-end
+
+    # Simple tests that check if Trotterization executes properly...
+    @testset "execution" begin 
+        H = OpList(Qubits(), 20)
+        for i = 1:20
+            add!(H, "x", i, 0.34)
+        end
+        for i = 1:19
+            add!(H, ["z", "z"], [i, i+1], 0.56)
+        end
+        for i = 1:18
+            add!(H, ["z", "x", "z"], [i, i+1, i+2], -1.4)
+        end
+
+        @test begin 
+            circ = TeNe._trotter_compressed_first_order(H, -1im*0.1)
+            length(circ.layers) == 3
+        end
+
+        @test begin 
+            circ = TeNe._trotter_compressed_second_order(H, -1im*0.1)
+            length(circ.layers) == 5
+        end
+
+        @test begin 
+            circ = TeNe._trotter_uncompressed_first_order(H, -1im*0.1)
+            length(circ.layers) == 6
+        end
+        @test begin 
+            circ = TeNe._trotter_uncompressed_second_order(H, -1im*0.1)
+            length(circ.layers) == 11
+        end
+        @test begin 
+            circ = trotterize(H, -0.01im)
+            length(circ.layers) == 5
+        end
+    end
+end 
