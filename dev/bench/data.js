99ee84e46cded923a98f566c229443c59bab337b
github-action-benchmark <github@users.noreply.github.com>
2024-11-07 21:27:11 +0000

    add Julia benchmark result (julia) benchmark result for 374c869ebdf80b3d2afb2201a7fd62ccda94e178

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1731014831850,
+  "repoUrl": "https://github.com/lcauser/TeNe.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "luke.causer@outlook.com",
+            "name": "Luke Causer",
+            "username": "lcauser"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "374c869ebdf80b3d2afb2201a7fd62ccda94e178",
+          "message": "Merge pull request #64 from lcauser/lc/lioville_wrapper\n\nLiouville Wrappers",
+          "timestamp": "2024-11-07T21:24:16Z",
+          "tree_id": "fc28494fd453e89c1bef8a244fde46eade77b121",
+          "url": "https://github.com/lcauser/TeNe.jl/commit/374c869ebdf80b3d2afb2201a7fd62ccda94e178"
+        },
+        "date": 1731014831549,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "dmrg",
+            "value": 61263953.5,
+            "unit": "ns",
+            "extra": "gctime=2431332.5\nmemory=28135072\nallocs=301932\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "state_optimiser",
+            "value": 106150443,
+            "unit": "ns",
+            "extra": "gctime=2940829\nmemory=32399504\nallocs=459037\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
