e8a85f4e68b8fc6c1df5c6b4f4048367d52598e6
github-action-benchmark <github@users.noreply.github.com>
2024-11-08 21:39:46 +0000

    add Julia benchmark result (julia) benchmark result for 4f7b4e1ba92aeaabd680d905788c5b9e597c675b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731014831850,
+  "lastUpdate": 1731101986607,
   "repoUrl": "https://github.com/lcauser/TeNe.jl",
   "entries": {
     "Julia benchmark result": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 106150443,
             "unit": "ns",
             "extra": "gctime=2940829\nmemory=32399504\nallocs=459037\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "luke.causer@outlook.com",
+            "name": "Luke Causer",
+            "username": "lcauser"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4f7b4e1ba92aeaabd680d905788c5b9e597c675b",
+          "message": "Merge pull request #65 from lcauser/lc/fix_wrapper\n\nfixed wrapper",
+          "timestamp": "2024-11-08T21:38:00Z",
+          "tree_id": "b46048f7db8de275ecccd53455823ebba1f320de",
+          "url": "https://github.com/lcauser/TeNe.jl/commit/4f7b4e1ba92aeaabd680d905788c5b9e597c675b"
+        },
+        "date": 1731101985540,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "dmrg",
+            "value": 59873818.5,
+            "unit": "ns",
+            "extra": "gctime=1906319\nmemory=28135072\nallocs=301932\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "state_optimiser",
+            "value": 100185454,
+            "unit": "ns",
+            "extra": "gctime=2514654\nmemory=36222112\nallocs=512368\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
